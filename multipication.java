efbf704e101a4c18536b3849246fa71332b6307e
kambhampati888 <69984925+kambhampati888@users.noreply.github.com>
2021-11-07 17:04:36 +0530

    Update multipication.java

--- multipication.java
+++ multipication.java
@@ -5,7 +5,7 @@ public class multipication {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter first number: ");
+        System.out.print("Enter main number: ");
 
      
         int num1 = scan.nextInt();
